26cd1190697230d816d26c19131238840a03e558
Dakota <prattdakota12@gmail.com>
2021-11-29 18:02:46 -0500

    Etch-a-Sketch logic and functionallity added

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 // Create a webpage with a 16x16 grid of square divs.
-const container = document.getElementById('container');
+const container = document.querySelector('.container');
 
-function amountOfDivs (rows, columns) {
-   for (let i = 0; i < rows * columns; i++) {
+function createDivs(rows, columns) {
+    for (let i = 0; i < (rows * columns); i++) {
         var div = document.createElement('div');
-        div.setAttribute('id', 'divs')
+        container.appendChild(div).className = 'cell';
         div.textContent = i + 1;
-        container.appendChild(div);
-    } 
+        div.id = 'cell ' + `${i}`;
+    }
+}
+createDivs(16, 16);
+
+function addEventListenerList(list, event, fn) {
+    for (var i = 0, len = list.length; i < len; i++) {
+        list[i].addEventListener(event, fn);
+    }
 }
 
-amountOfDivs(16, 16);
+//Nodelist of the divs (cells) created with the createDivs function.
+const cells = document.querySelectorAll('.cell'); 
 
+//Function that loops through the nodelist and adds an eventListener to each element.
+addEventListenerList(cells, 'mouseenter', changeColor);
 
+//Function that gets called by the eventListener to change backgroundColor in Javascript.
+function changeColor(e) {
+    console.log(e.target.textContent);
+    // e.target.className = 'triggered';
+    e.target.style.backgroundColor = 'black';
+}
