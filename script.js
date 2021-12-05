ef8aa108be414c2ec4d0a3c41f6a3a15e0bdf579
Dakota <prattdakota12@gmail.com>
2021-12-05 18:47:54 -0500

    All functionality added and working.

--- script.js
+++ script.js
@@ -1,45 +1,91 @@
-// Create a webpage with a 16x16 grid of square divs.
+//Container to hold all cells
 const container = document.querySelector('.container');
 
-function createDivs(rows, columns) {
+//Nodelist of the cells created with the createDivs function
+let cells = document.querySelectorAll('.cell');
+
+//Default grid
+createCells(32, 32, '3.125%');
+activateCells();
+
+//h2 element showing the current grid size
+let gridText = document.getElementById('size');
+
+            /* BUTTONS */
+
+//RESET button
+const resetButton = document.querySelector('.resetButton');
+resetButton.addEventListener('click', resetButtonFunction);
+function resetButtonFunction() {
+    alert('RESET');
+    removeStyleList(cells);
+}
+
+//GRIDSIZE button
+const sizeButton = document.querySelector('.gridButton');
+sizeButton.addEventListener('click', gridButtonFunction);
+function gridButtonFunction() {
+    let answer = prompt('How big do you want your grid? 16x16, 32x32, or 64x64.', '16x16')
+    if (answer == '16x16') {
+        clearGrid();
+        createCells(16, 16, '6.25%');
+        activateCells();
+        gridText.textContent = '16x16';
+    } else if (answer == '32x32') {
+        clearGrid();
+        createCells(32, 32, '3.125%');
+        activateCells();
+        gridText.textContent = '32x32';
+    } else if (answer == '64x64') {
+        clearGrid();
+        createCells(64, 64, '1.5625%');
+        activateCells();
+        gridText.textContent = '64x64';
+    } else {
+        answer = 'User cancelled the prompt.';
+    }
+}
+
+            /* FUNCTIONS */
+
+//Function to create cells for the grid
+function createCells(rows, columns, size) {
     for (let i = 0; i < (rows * columns); i++) {
         var div = document.createElement('div');
         container.appendChild(div).className = 'cell';
         div.id = 'cell ' + `${i}`;
+        div.style.width = size;
     }
 }
-createDivs(16, 16);
 
+//Function that loops through the nodelist and adds an eventListener to each element.
 function addEventListenerList(list, event, fn) {
     for (var i = 0, len = list.length; i < len; i++) {
         list[i].addEventListener(event, fn);
     }
 }
 
-function removeStyleList(list) {
-    for (var i = 0, len = list.length; i < len; i++) {
-        list[i].style.removeProperty('background-color');
-    }
+//Function to apply eventListener to all cells in the grid
+function activateCells() {
+    cells = document.querySelectorAll('.cell'); 
+    addEventListenerList(cells, 'mouseenter', changeColor);
 }
-
-//Nodelist of the divs (cells) created with the createDivs function.
-const cells = document.querySelectorAll('.cell'); 
-
-//Function that loops through the nodelist and adds an eventListener to each element.
-addEventListenerList(cells, 'mouseenter', changeColor);
 
 //Function that gets called by the eventListener to change backgroundColor in Javascript.
 function changeColor(e) {
-    // e.target.className = 'triggered';
     e.target.style.backgroundColor = 'black';
 }
 
-//Button logic
-const button = document.querySelector('.button');
+//Function to clear grid
+function clearGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
 
-button.addEventListener('click', buttonFunction);
-
-function buttonFunction() {
-    removeStyleList(cells);
-    alert('Canvas cleared!');
+//Function to reset grid by looping through the node list of divs and changing the background of each element/node
+function removeStyleList(list) {
+    for (var i = 0, len = list.length; i < len; i++) {
+        list[i].style.backgroundColor = 'darksalmon';
+    }
 }
